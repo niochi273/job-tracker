0bc94989845745c7d5799352a60767a006d7fbdf
niochi273 <valeraveselovsky2000@gmail.com>
2026-06-10 23:17:03 +0700

    refactor: add shadcn card component

--- src/components/ApplicationCard.tsx
+++ src/components/ApplicationCard.tsx
@@ -1,36 +1,41 @@
-import { Card } from "./ui/card";
+import { Card, CardTitle, CardHeader, CardContent } from "./ui/card";
 import { Application } from "@/types/application";
 import { format } from "date-fns";
 import StatusBadge from "./StatusBadge";
+import Link from "next/link";
 
 export default function ApplicationCard({ app }: { app: Application }) {
   return (
     <>
-      <Card className="p-4 flex flex-col gap-3">
-        <div className="flex flex-row gap-5 items-center">
-          <h2 className="text-3xl font-medium">{app.company}</h2>
+      <Card className="p-4 flex flex-col relative gap-3  hover:shadow transition-shadow duration-100">
+        <CardHeader className="flex flex-row gap-5 items-center">
+          <Link href={`/applications/${app.id}`}>
+            <CardTitle className="text-3xl">{app.company}</CardTitle>
+          </Link>
           <StatusBadge status={app.status} />
           <span className="ml-auto text-muted-foreground">
             {format(app.appliedAt, "d MMM yyyy")}
           </span>
-        </div>
-        <p>{app.position}</p>
-        <p className={!app.salaryRange ? "text-gray-400" : ""}>
-          {app.salaryRange ?? "Salary undisclosed"}
-        </p>
-        <p>{app.workType}</p>
-        {app.location != null && <p>{app.location}</p>}
+        </CardHeader>
+        <CardContent>
+          <p>{app.position}</p>
+          <p className={!app.salaryRange ? "text-gray-400" : ""}>
+            {app.salaryRange ?? "Salary undisclosed"}
+          </p>
+          <p className="capitalize">{app.workType}</p>
+          {app.location != null && <p>{app.location}</p>}
+        </CardContent>
+        {app.jobUrl && (
+          <a
+            href={app.jobUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm text-violet-400 hover:underline absolute bottom-3 right-6"
+          >
+            View job posting →
+          </a>
+        )}
       </Card>
-      <a
-        href={app.jobUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-violet-400 hover:underline absolute bottom-2 right-5"
-      >
-        View job →
-      </a>
     </>
   );
 }
-
-// компанию (крупно), позицию, статус (как Badge), локацию, тип работы, дату подачи
