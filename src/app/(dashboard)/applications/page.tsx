6f47efb63308af8b3159eb7dbaeef25b04dea6a9
niochi273 <valeraveselovsky2000@gmail.com>
2026-06-17 21:03:44 +0700

    feat: add status filter for applications

--- src/app/(dashboard)/applications/page.tsx
+++ src/app/(dashboard)/applications/page.tsx
@@ -1,4 +1,5 @@
 import ApplicationCard from "@/components/ApplicationCard";
+import StatusFilter from "@/components/StatusFilter";
 import { db } from "@/db";
 import { applications } from "@/db/schema";
 import { auth } from "@/lib/auth";
@@ -7,22 +8,33 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function ApplicationsPage() {
+export default async function ApplicationsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ status?: string }>;
+}) {
+  const { status } = await searchParams;
   const statuses: Partial<Record<ApplicationStatus, number>> = {};
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  // layout уже гарантирует, что session есть, но TS этого не знает:
+
   if (!session) {
     redirect("/login");
   }
 
   const userApplications: Application[] = await db.query.applications.findMany({
-    where: eq(applications.userId, session.user.id), // ← фильтр по юзеру
+    where: eq(applications.userId, session.user.id),
     orderBy: (apps, { desc }) => desc(apps.appliedAt),
   });
 
-  for (const app of userApplications) {
+  const userFilteredApplications =
+    status != null
+      ? userApplications.filter((app) => app.status === status)
+      : userApplications;
+
+  for (const app of userFilteredApplications) {
     statuses[app.status] = (statuses[app.status] ?? 0) + 1;
   }
 
@@ -30,8 +42,11 @@ export default async function ApplicationsPage() {
     <main className="w-full mx-auto p-6">
       <div className="flex items-center gap-3 mb-6">
         <h1 className="text-3xl font-bold">Applications</h1>
-        {Object.keys(statuses).length !== 0 && (
-          <div className="mt-1 border rounded-2xl py-1 px-2 flex gap-3">
+        <StatusFilter
+          validStatuses={userApplications.map((app) => app.status)}
+        />
+        {Object.keys(statuses).length !== 0 && status != "" && (
+          <div className="mt-1 ml-auto border rounded-2xl py-1 px-2 flex gap-3">
             {Object.entries(statuses).map(([status, count]) => (
               <span className="capitalize text-sm" key={status}>
                 {status}: {count}
@@ -41,13 +56,13 @@ export default async function ApplicationsPage() {
         )}
       </div>
 
-      {userApplications.length === 0 ? (
+      {userFilteredApplications.length === 0 ? (
         <p className="text-muted-foreground text-center py-12">
           No applications yet. Add your first one to get started.
         </p>
       ) : (
         <div className="flex flex-col gap-3">
-          {userApplications.map((app) => (
+          {userFilteredApplications.map((app) => (
             <ApplicationCard key={app.id} app={app} />
           ))}
         </div>
---
+++ src/components/StatusFilter.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+
+export default function StatusFilter({
+  validStatuses,
+}: {
+  validStatuses: readonly string[];
+}) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const status = searchParams.get("status");
+  console.log(status);
+
+  return (
+    <>
+      <Select
+        value={status ?? ""}
+        onValueChange={(v) =>
+          router.push(v ? `/applications?status=${v}` : "/applications")
+        }
+      >
+        <SelectTrigger className="mt-1">
+          <SelectValue placeholder="Status" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {validStatuses.map((validStatus, i) => (
+              <SelectItem
+                className="cursor-pointer"
+                key={i}
+                value={validStatus}
+              >
+                {validStatus}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      {status != null && (
+        <Button
+          onClick={() => router.push("/applications")}
+          variant="destructive"
+          className="mt-1"
+        >
+          <X />
+        </Button>
+      )}
+    </>
+  );
+}
